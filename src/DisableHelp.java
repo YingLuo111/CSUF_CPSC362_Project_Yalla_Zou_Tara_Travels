5478f3a1c4fad29ed660b5cfde8bf46465b482d4
Donavie Ordonez <47704302+donavielove@users.noreply.github.com>
2020-05-02 14:09:37 -0700

    Update DisableHelp.java

--- src/DisableHelp.java
+++ src/DisableHelp.java
@@ -1,18 +1,21 @@
 /*
- * This the sub JPanel for the JPanel  Disabled help
+ * This the sub JPanel for the JPanel Disabled help
  */
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-//This is the class of the JPanel DisableHelp and implements ActionListener
+
+//This is the class of the JPanel DisableHelp. It mplements ActionListener and ITemListener
 public class DisableHelp extends JPanel implements ActionListener, ItemListener{
-	//Initialize all of the JLable, JButton,JComboBox and JPanel
+	
+	// Initializing all of the JLabels, JButtons,  JPanels, 
+	// ImageIcons, Images, and JCheckBoxes 
 	
 	private JLabel label1            = new JLabel("The Disabled Help Service ");
-	private JLabel label2            = new JLabel(" Plaese choice");
-	private JLabel label3            = new JLabel(" what service you want to reserve.V");
-	private JButton finish             = new JButton("finish or Skip to next -->>");
-	private JButton backMoreSevice       = new JButton("<<--Back More Sevice");
+	private JLabel label2            = new JLabel(" Please Select a Service");
+	private JLabel label3            = new JLabel(" What service do you want to reserve? ");
+	private JButton finish             = new JButton(" Finish or Skip to next -->>");
+	private JButton backMoreSevice       = new JButton("<<--Back More Service");
 	private JButton backHome         = new JButton("<<--Back Home");
 	
 	private JLabel image  = new JLabel(" ");
@@ -26,7 +29,7 @@ public class DisableHelp extends JPanel implements ActionListener, ItemListener{
 	private JPanel panelE = new JPanel();
 	private JPanel panelS = new JPanel();
 	
-    private ImageIcon Picture;
+ 	private ImageIcon Picture;
 	private Image Img;
 	private ImageIcon Picture2;
 	private Image Img2;
@@ -38,11 +41,11 @@ public class DisableHelp extends JPanel implements ActionListener, ItemListener{
 	private JCheckBox Wheelchairs;
 	private JCheckBox Hearingaids;
 	
+	// Making a container of type Fly
 	private Fly flyContainer;
 	/*
-	 * This is the constructor  of this class
-	 *this consturctor setting the all of the JLabel's
-	 *JButton's, JPanel's font,backgound, size and layout 
+	 * This is the constructor of this class. It sets all of the JLabels,
+	 * JButtons, JPanels, ImageIcons, JCheckboxes font, backgound, size and layout 
 	 */
 	public DisableHelp(Fly container) {
 		
@@ -50,9 +53,11 @@ public class DisableHelp extends JPanel implements ActionListener, ItemListener{
 		setSize(800,800);
 		setLayout(new BorderLayout());
 		
-		label1.setFont(new Font("Arial", Font.BOLD, 25));
+		Font arial25 = new Font("Arial", Font.BOLD, 25);
+	
+		label1.setFont(arial25);
 		setJLableBackGround(label1, Color.BLACK,new Color(255,240,245));
-		label2.setFont(new Font("Arial", Font.BOLD, 25));
+		label2.setFont(arial25);
 		setJLableBackGround(label1, Color.BLACK,new Color(255,240,245));
 		label3.setFont(new Font("Arial", Font.BOLD, 15));
 		setJLableBackGround(label1, Color.BLACK,new Color(255,240,245));
@@ -73,12 +78,11 @@ public class DisableHelp extends JPanel implements ActionListener, ItemListener{
 		Img3 = Picture3.getImage().getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH);
 		image3.setIcon(new ImageIcon(Img3));
 		
-	    crutches    = new JCheckBox("Crutches, Canes, and Walkers",false);
+	        crutches    = new JCheckBox("Crutches, Canes, and Walkers",false);
 		Braces      = new JCheckBox("Braces/Prosthetics",false);
 		Wheelchairs = new JCheckBox("Wheelchairs",false);
 		Hearingaids = new JCheckBox("Hearing aids",false);
-		
-		
+	
 		main.setLayout(new BorderLayout());
 		
 		panelN.add(image);
@@ -117,46 +121,42 @@ public class DisableHelp extends JPanel implements ActionListener, ItemListener{
 		crutches.addItemListener(this);
 		Braces.addItemListener(this);
 		Wheelchairs.addItemListener(this);
-		Hearingaids.addItemListener(this);
-		
+		Hearingaids.addItemListener(this);	
 	}
+	
 	/*
-	 * This is the actionperformed to execute the button's action 
-	 * if the user click the different ItemSelect go to the  different Select Item
+	 * This is the itemStateChanged. If an item is selected, it is saved 
+	 * into the current user's info for their flight
 	 */
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		Object source = e.getItem();
-	    int select = e.getStateChange();
-
-	    if (source == crutches) {
-	               if(select == ItemEvent.SELECTED) {   
-	     
-	    }else if (source == Braces) {
-	               if(select == ItemEvent.SELECTED) {
-	           	   
-	               }
-	    }else if(source == Wheelchairs) {
-	    	  if(select == ItemEvent.SELECTED) {
-	    		  
-	    	  }
-	    }else if (source == Hearingaids) {
-	    	   if (select == ItemEvent.SELECTED) {
-	    		   
-	    	   }
-	    }
-	    	
-	    }
+		int select = e.getStateChange();
 		
+		if (source == crutches) {
+			if(select == ItemEvent.SELECTED) {   
+	    		}
+	    	else if (source == Braces) {
+	    		if(select == ItemEvent.SELECTED) {
+	        	}
+	    	}
+	    	else if(source == Wheelchairs) {
+	    		if(select == ItemEvent.SELECTED) {
+	    		}
+	    	}
+	    	else if (source == Hearingaids) {
+	    		if (select == ItemEvent.SELECTED) {
+	    		}
+	    	}	
+	    }	
 	}
-	/*
-	 * This is the actionperformed to execute the button's action 
-	 * if the user click the different buttonSelect go to the  different Select actions
-	 */
+
+	// This is the actionperformed method to execute the button's action when selected 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-        Object source = e.getSource();
+		Object source = e.getSource();
 		
+		// If user selects the "Finish" button, babyHelpPanel appears 
 		if(source == finish){
 			CardLayout flyCardLayout = flyContainer.getCardLayout();
 			flyCardLayout.show(flyContainer.getContentPane(),"babyHelpPanel");
@@ -174,25 +174,25 @@ public class DisableHelp extends JPanel implements ActionListener, ItemListener{
 			if (Hearingaids.isSelected()) {
 				curService.addDisableFacility("Hearing aids");
 			} 
-		}else if(source == backMoreSevice) {
-			
-			CardLayout flyCardLayout = flyContainer.getCardLayout();
-			flyCardLayout.show(flyContainer.getContentPane(),"seviceHomePanel");
-			
-		}else if(source == backHome) {
-			
-			CardLayout flyCardLayout = flyContainer.getCardLayout();
-			flyCardLayout.show(flyContainer.getContentPane(), "HomePanel");
-			//reset the information
-			flyContainer.reset();
 		}
 		
+		// If user selects the "Back More Service" button, serviceHomePanel appears 
+		else if(source == backMoreSevice) {
+			CardLayout flyCardLayout = flyContainer.getCardLayout();
+			flyCardLayout.show(flyContainer.getContentPane(),"seviceHomePanel");
+		}
+		
+		// If user selects the "Back Home" button, "HomePanel" appears 
+		else if(source == backHome) {
+			CardLayout flyCardLayout = flyContainer.getCardLayout();
+			flyCardLayout.show(flyContainer.getContentPane(), "HomePanel");
+			
+			// The information resets 
+			flyContainer.reset();
+		}
 	}
-	/*
-	 * This is the method to set the JButton Background 
-	 * This is the method to set the JLabel Background 
-	 * This is the method to set the JTextArea Background 
-	 */
+	
+	// Method to set JButton background
 	public void setJButtonBackGround(JButton b, Color FC,Color BC) {
 		
 		b.setForeground(FC);       
@@ -200,24 +200,27 @@ public class DisableHelp extends JPanel implements ActionListener, ItemListener{
 		b.setOpaque(true);               
 		b.setBorderPainted(false);        
 	}
-   public void setJLableBackGround(JLabel l, Color FC,Color BC) {
+	
+	// Method to set JLabel background
+  	public void setJLableBackGround(JLabel l, Color FC,Color BC) {
 		l.setOpaque(true);  
 		l.setBackground(BC);
-		l.setForeground(FC);
-		
+		l.setForeground(FC);	
 	}
-   public void setJTextAreaBackGround(JTextArea t, Color FC,Color BC) {
+	
+	// Method to set JTextArea background
+	public void setJTextAreaBackGround(JTextArea t, Color FC,Color BC) {
 		t.setOpaque(true);  
 		t.setBackground(BC);
 		t.setForeground(FC);
 		
 	}
-   //This is the reset mothed to reset the information
-   public void reset() {
-	    crutches.setSelected(false);   
+  
+	// Method to reset the information
+   	public void reset() {
+		crutches.setSelected(false);   
 		Braces.setSelected(false);      
 		Wheelchairs.setSelected(false); 
 		Hearingaids.setSelected(false); 
-   }
-
+	}
 }
