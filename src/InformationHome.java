2c5cf5ed378d5d98065a3cd9f25814dbf75542f3
Donavie Ordonez <47704302+donavielove@users.noreply.github.com>
2020-05-02 17:47:40 -0700

    Update InformationHome.java

--- src/InformationHome.java
+++ src/InformationHome.java
@@ -1,57 +1,62 @@
 /*
  * This is the class of the information homeJPanel 
- * this class set the some choice button
- * let the user can choice any of them to go to 
- * subJpanle of the Information JPanle
- * and can enter or select what thing they want 
- * booking or to see or want to choice.
+ * this class sets the buttons users can choose from.
  */
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-//This is the class of the JPanel InformationHome and implements ActionListener
+// This is the class of the JPanel "InformationHome". It implements ActionListener
 public class InformationHome extends JPanel implements ActionListener{
-	//Initialize the JLable 
-	private JLabel label1 = new JLabel("             Please choice one. ");
-	//Initialize the JButton
+	
+	// Initializing all of the JLabels, JButtons, JPanels, 
+	// ImageIcons and Images
+	private JLabel label1 = new JLabel("             Please Select One. ");
+	
 	private JButton luggage    = new JButton("Luggage");
 	private JButton wheather   = new JButton("Weather");
 	private JButton  seat      = new JButton("Select Seat");
 	private JButton backHome   = new JButton("<<--Back Service");
-	//Initialize the JLabel
+	
 	JLabel image  = new JLabel(" ");
 	JLabel image2 = new JLabel(" ");
 	JLabel image3 = new JLabel(" ");
-	//Initialize the JPanel
+	
 	JPanel main   = new JPanel();
 	JPanel panelC = new JPanel();
 	JPanel panelN = new JPanel();
 	JPanel panelW = new JPanel();
 	JPanel panelS = new JPanel();
-	//Initialize the Image and Image
-    private ImageIcon Picture;
+	
+	private ImageIcon Picture;
 	private Image Img;
 	private ImageIcon Picture2;
 	private Image Img2;
 	private ImageIcon Picture3;
 	private Image Img3;
 	
+	// Making a container of type "Fly"
 	private Fly flyContainer;
-	//constuctor the  InformationHome
+	
+	/*
+	 * This is the constructor of this class.
+	 * The consturctor sets all of the JLabels, JButtons, 
+	 * JPanels, ImageIcons, font, backgound, size and layout 
+	 */
 	public InformationHome(Fly container) {
-		//setting the Layout and Size
+		
 		flyContainer = container;
 		setSize(800,800);
 		setLayout(new BorderLayout());
-		//setting the label1 font
+		
 		 label1.setFont(new Font("Arial", Font.BOLD, 25));
-		//setting the JButton's and JLabel's background
+		
 		 setJLableBackGround(label1, Color.BLACK,new Color(135,206,250));
 		 setJButtonBackGround(luggage, Color.BLACK,new Color(135,206,250));
 		 setJButtonBackGround(seat, Color.BLACK,new Color(135,206,250));	
 		 setJButtonBackGround(wheather, Color.BLACK,new Color(135,206,250));
 		 setJButtonBackGround(backHome, Color.BLACK,new Color(135,206,250));
-		//setting the the picture fxed the size  adding the Icon to each picture
+		
 		Picture = new ImageIcon("resources/Images/subtitle.png");
 		Img = Picture.getImage().getScaledInstance(900, 250, java.awt.Image.SCALE_SMOOTH);
 		image.setIcon(new ImageIcon(Img));
@@ -63,12 +68,6 @@ public class InformationHome extends JPanel implements ActionListener{
 		Picture3 = new ImageIcon("resources/Images/selectImageWest.png");
 		Img3 = Picture3.getImage().getScaledInstance(300, 400, java.awt.Image.SCALE_SMOOTH);
 		image3.setIcon(new ImageIcon(Img3));
-		
-		/*
-		* setting the subPanel's layout and adding the button label to each one
-		* setting the PanelW/panelN/PanelS/PanelC
-		* and adding all of the button and label to dfferent part
-		*/
 		
 		panelW.setBackground(new Color(135,206,250));
 		main.setLayout(new BorderLayout());
@@ -89,57 +88,57 @@ public class InformationHome extends JPanel implements ActionListener{
 		main.add(panelC, BorderLayout.CENTER);
 		
 		add(main);
-		//Adding the action listenner for each button
+		
 		luggage.addActionListener(this);
 		wheather.addActionListener(this);
 		seat.addActionListener(this);
 		backHome.addActionListener(this);
-		
 	}
-	/*
-	 * This is the actionperformed to execute the button's action 
-	 * if the user click the different buttonSelect go to the  different Select subPanel
-	 */
+	
+	// This is the actionperformed method to execute the button's action when selected 
 	@Override
 	public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
-		
+		// If user selects the "luggage" button, "LuggagePanel" appears
 		if(source == luggage){
 			CardLayout flyCardLayout = flyContainer.getCardLayout();
 			flyCardLayout.show(flyContainer.getContentPane(),"LuggagePanel");
-		}else if(source == wheather){
+		}
+
+		// If user selects the "weather" button. "weatherPanel" appears
+		else if(source == wheather){
 			CardLayout flyCardLayout = flyContainer.getCardLayout();
 			flyCardLayout.show(flyContainer.getContentPane(), "wheatherPanel");
-		}else if(source == seat) {
-			CardLayout flyCardLayout = flyContainer.getCardLayout();
-			flyCardLayout.show(flyContainer.getContentPane(),"seatPanel");
-		}else if(source == backHome) {
-			CardLayout flyCardLayout = flyContainer.getCardLayout();
-			flyCardLayout.show(flyContainer.getContentPane(), "HomePanel");
-			//reset the information
-			flyContainer.reset();
 		}
 		
+		// If user selects the "seat" button, "seatPanel" appears
+		else if(source == seat) {
+			CardLayout flyCardLayout = flyContainer.getCardLayout();
+			flyCardLayout.show(flyContainer.getContentPane(),"seatPanel");
+		}
+		
+		// If user selects the "backHome" button, "HomePanel" appears
+		else if(source == backHome) {
+			CardLayout flyCardLayout = flyContainer.getCardLayout();
+			flyCardLayout.show(flyContainer.getContentPane(), "HomePanel");
+			
+			// Information resets
+			flyContainer.reset();
+		}	
 	}
-	/*
-	 * This is the method to set the JButton Background 
-	 */
-	 public void setJButtonBackGround(JButton b, Color FC,Color BC) {
-			
-			b.setForeground(FC);       
-			b.setBackground(BC);       
-			b.setOpaque(true);               
-			b.setBorderPainted(false);        
-		}
-   /*
-	* This is the method to set the JLabel Background 
-    */
-	   public void setJLableBackGround(JLabel l, Color FC,Color BC) {
-			l.setOpaque(true);  
-			l.setBackground(BC);
-			l.setForeground(FC);
-			
-		}
-	  
+	
+	// Method to set JButton background
+	public void setJButtonBackGround(JButton b, Color FC,Color BC) {
+		b.setForeground(FC);       
+		b.setBackground(BC);       
+		b.setOpaque(true);               
+		b.setBorderPainted(false);        
+	}
+	
+	// Method to set JLabel background 
+	public void setJLableBackGround(JLabel l, Color FC,Color BC) {
+		l.setOpaque(true);  
+		l.setBackground(BC);
+		l.setForeground(FC);
+	}  
 }
-
