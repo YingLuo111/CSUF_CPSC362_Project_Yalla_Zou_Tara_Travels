918506b329438f257bc8f115d90de689edda005a
Donavie Ordonez <47704302+donavielove@users.noreply.github.com>
2020-05-05 15:21:51 -0700

    Update Signup.java

--- src/Signup.java
+++ src/Signup.java
@@ -1,10 +1,3 @@
-/*
- * This the Class of main JPanel Home
- * Home calss extends JPanel
- * This class include the four subhome Button
- * and it can go one of the other sub four home class
- * by click button
- */
 import javax.swing.*;
 
 import java.awt.*;
@@ -13,14 +6,15 @@ import java.awt.image.BufferedImage;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-/*
- * This is the Home class
- */
+
+// This is the Signup class. It uses ActionListener 
 public class Signup extends JPanel implements ActionListener{
-	//Initialize the JPanel panelHome and panelButton
+	
+	// Initializing the JPanels, JLabels, JTextfields, JButtons
+	// ImageIcons and Images 
 	private JPanel panelHome         = new JPanel();
 	private JPanel panelSignup       = new JPanel();
-    //Initialize the JLabel title
+ 
 	private JLabel title             = new JLabel();
 	private JLabel username          = new JLabel("     Username >>>");
 	private JLabel password    		 = new JLabel("     Password >>>");
@@ -29,73 +23,74 @@ public class Signup extends JPanel implements ActionListener{
 	private JTextField passText 	 = new JTextField("",20);
 	private JTextField emailText 	 = new JTextField("",20);
 	
-	//Initialize the JButton 
-	private JButton buttonSignup     = new JButton("Signup");
+	private JButton buttonSignup     = new JButton("Sign Up");
 	private JButton buttonBack       = new JButton("Back");
 
-	//Initialize the JLabel for insert the image
 	private JLabel image             = new JLabel(" ");
 	private JLabel image2            = new JLabel(" ");
-	//Initialize the ImageIcon and Image
+
     private ImageIcon Picture;
 	private Image Img;
 	private ImageIcon Picture2;
 	private Image Img2;
-	//Initialize the JFrame flyContainer is the Fly
+	
+	// Creating a variable flycontainer of type Fly
 	private Fly flyContainer;
 	
+	// Creating a variable dbmgr of type DBManager
 	private DBManager dbmgr;
 	
 	private String passwordStr = "";
 	
 	CardLayout cardLayout = new CardLayout();
-	//This is the constructor
+	
+	/*
+	 * This is the constructor of the class, it uses the input for classes
+	 * Fly and DBManager. It sets all of the JLabels, JButtons, JPanels, 
+	 * Images, fonts, backgrounds, size and layout.
+	 */
 	public Signup(Fly container, DBManager dbmgr) {
-		//setting the Layout and Size
+	
 		flyContainer = container;
-		
 		this.dbmgr = dbmgr;
 		
 		setLayout(cardLayout);
 		setSize(1000,800);
-		//create the title and set the font and background
+	
 		title = new JLabel("        Yalla Zou Tara Travels Login ");
 	    title.setFont(new Font("PilGi", Font.BOLD, 40));
 	    setJLableBackGround(title, Color.BLACK,new Color(135,206,250));
-	    
-	    //setting the Button's font 
-	    buttonBack.setFont(new Font("Arial", Font.BOLD, 25));
+	
+	    Font arial25 = new Font("Arial", Font.BOLD, 25);
+	    buttonBack.setFont(arial25);
 	    buttonBack.setToolTipText("返回\n"+ " بازگشت");
-	    buttonSignup.setFont(new Font("Arial", Font.BOLD, 25));
+	    buttonSignup.setFont(arial25);
 	    buttonSignup.setToolTipText("注册\n"+ "ثبت نام");
 	    
-
-	    username.setFont(new Font("Arial", Font.BOLD, 25));
+	    username.setFont(arial25);
 	    username.setToolTipText("用户名\n"+ "نام کاربری");
 	    
-	    password.setFont(new Font("Arial", Font.BOLD, 25));
+	    password.setFont(arial25);
 	    password.setToolTipText("密码\n" + "کلمه عبور");
 	    
-	    email.setFont(new Font("Arial", Font.BOLD, 25));
+	    email.setFont(arial25);
 	    email.setToolTipText("电子邮箱\n" + "پست الکترونیک");
 	    
 	    setJLableBackGround(username, Color.BLACK,new Color(176,224,230));
 	    setJLableBackGround(password, Color.BLACK,new Color(95,158,160));
 	    setJLableBackGround(email, Color.BLACK,new Color(32,178,170));
 
-        //settong the Button's background color
 	    setJButtonBackGround(buttonBack, Color.BLACK,Color.YELLOW);
 	    setJButtonBackGround(buttonSignup, Color.BLACK,Color.PINK);
 
-	    //Creating the ImageIcon and fixed it size
         Picture = new ImageIcon("resources/Images/HomeImagenorth.png");
 		Img = Picture.getImage().getScaledInstance(900, 300, java.awt.Image.SCALE_SMOOTH);
 		image.setIcon(new ImageIcon(Img));
-		//Creating the ImageIcon and fixed it size
+	
 		Picture2 = new ImageIcon("resources/Images/HomeImageSorth.png");
 		Img2 = Picture2.getImage().getScaledInstance(900, 300, java.awt.Image.SCALE_SMOOTH);
 		image2.setIcon(new ImageIcon(Img2));
-		//setting the button's layout and adding it
+		
 		panelSignup.setLayout(new GridLayout(4, 2));
 		panelSignup.add(username);
 		panelSignup.add(userText);
@@ -105,17 +100,19 @@ public class Signup extends JPanel implements ActionListener{
 		panelSignup.add(emailText);
 		panelSignup.add(buttonBack);
 		panelSignup.add(buttonSignup);
-		//setting the panelHome's layout and adding it
+	
 		panelHome.setLayout(new GridLayout(4, 1));
 		panelHome.add(image);
 		panelHome.add(title); 
 		panelHome.add(panelSignup);
 		panelHome.add(image2);
 		add(panelHome,"Panel1");
-		//Adding the button's action
+		
 		buttonBack.addActionListener(this);
 		buttonSignup.addActionListener(this);
 		
+		// Key listener method to hide the user's password input 
+		// changing the characters to "*"
 	    KeyListener keyListener = new KeyListener() {
 	        public void keyPressed(KeyEvent keyEvent) {}
 
@@ -140,20 +137,20 @@ public class Signup extends JPanel implements ActionListener{
 	      passText.addKeyListener(keyListener);
 	
 	}
-	/*
-	 * This is the actionperformed to execute the button's action 
-	 * if click the buttonSelect go to the Select HomePanel
-	 * if click the buttonInfomation go to the Infomation HomePanel
-	 * if click the buttonSevice go to the Sevice HomePanel
-	 * if click the buttonPersinal go to the Persinal HomePanel
-	 */
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
+		
+		// When the user selects "Back" button, "LoginPanel" appears 
 		if(source == buttonBack){
 			CardLayout flyCardLayout = flyContainer.getCardLayout();
 			flyCardLayout.show(flyContainer.getContentPane(), "LoginPanel");
-		}else if(source == buttonSignup){
+		}
+		
+		// When the user selects "Sign Up" button, the user's input is read 
+		// by the program
+		else if(source == buttonSignup){
 			String username = userText.getText();
 			String password = passwordStr;
 			String email    = emailText.getText();
@@ -162,6 +159,7 @@ public class Signup extends JPanel implements ActionListener{
 				return;
 			}
 			
+			// If the user's inputs are valid, they sign up successfully
 			if (dbmgr.insertUserEntryToDB(username, password, email)) {
 				int clicked = JOptionPane.showOptionDialog(null, "Sign up succeed! Now navigate to log in.", "Congratulations!", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
 
@@ -170,31 +168,31 @@ public class Signup extends JPanel implements ActionListener{
 					CardLayout flyCardLayout = flyContainer.getCardLayout();
 					flyCardLayout.show(flyContainer.getContentPane(), "LoginPanel");
 				}
-			} else {
+			} 
+			
+			// If the user's inputs are invalid, the sign up is unsuccessful
+			else {
 				JOptionPane.showMessageDialog(null, "Sorry, sign up failed, please try again.");
 			}
 		}
 	}
-	/*
-	 * This is the method to set the JButton Background 
-	 */
+	
+	// Method to set JButton Background 
    public void setJButtonBackGround(JButton b, Color FC,Color BC) {
-		
 		b.setForeground(FC);       
 		b.setBackground(BC);       
 		b.setOpaque(true);               
 		b.setBorderPainted(false);        
 	}
-   /*
-	 * This is the method to set the JLabel Background 
-	 */
+  
+   // Method to set JLabel Background
    public void setJLableBackGround(JLabel l, Color FC,Color BC) {
 		l.setOpaque(true);  
 		l.setBackground(BC);
 		l.setForeground(FC);
-		
 	}
   
+   // Boolean method to check if the username is valid 
 	private boolean validateUsername(String username) {
 		boolean result = false;
 		if (username != null && username.length() > 0) {
@@ -202,10 +200,10 @@ public class Signup extends JPanel implements ActionListener{
 		} else {
 			JOptionPane.showMessageDialog(null, "Username could not be empty, please enter a username!");
 		}
-		
 		return result;
 	}
 	
+	// Boolean method to check if password is valid 
 	private boolean validatePassword(String password) {
 		boolean result = false;
 		if (password != null && password.length() > 0) {
@@ -213,10 +211,10 @@ public class Signup extends JPanel implements ActionListener{
 		} else {
 			JOptionPane.showMessageDialog(null, "Password could not be empty, please enter a password!");
 		}
-		
 		return result;
 	}
 	
+	// Boolean method to check if email is valid
 	private boolean validateEmail(String email) {
 		boolean result = false;
 		if (email != null && email.length() > 0) {
@@ -224,8 +222,6 @@ public class Signup extends JPanel implements ActionListener{
 		} else {
 			JOptionPane.showMessageDialog(null, "Email could not be empty, please enter an email!");
 		}
-		
 		return result;
 	}
-
 }
